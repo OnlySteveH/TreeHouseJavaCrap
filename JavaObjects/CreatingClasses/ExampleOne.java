e9ad8c62d03ee46ef131cb213932c26533a27c36
Roope Jourio <jourioroope@gmail.com>
2017-07-08 23:03:26 +0300

    small changes

--- JavaObjects/CreatingClasses/ExampleOne.java
+++ JavaObjects/CreatingClasses/ExampleOne.java
@@ -5,6 +5,8 @@ public class ExampleOne {
     System.out.printf("FUN FACT: There are %d PEZ allowed in every Dispenser %n", PezDispenser.MAX_PEZ);
     PezDispenser dispenser = new PezDispenser("Yoda");
     System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+    System.out.println("Filling the dispenser with delicious PEZ...");
+    dispenser.fill();
 
 
 
