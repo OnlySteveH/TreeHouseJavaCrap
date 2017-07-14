0a2503c308bf9e20a8d9f057b31e1f600464b696
Roope Jourio <jourioroope@gmail.com>
2017-07-14 19:47:49 +0300

    Added ability add the word as argument when executing.

--- JavaObjects/Hangman/Hangman.java
+++ JavaObjects/Hangman/Hangman.java
@@ -1,8 +1,13 @@
 public class Hangman {
 
   public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("Usage: Java Hangman <answer>");
+      System.err.println("answer is required");
+      System.exit(1);
+    }
 
-    Game game = new Game("treehouse");
+    Game game = new Game(args[0]);
     Prompter prompter = new Prompter(game);
 
     while (game.getRemainingTries() > 0 && !game.isWon()) {
