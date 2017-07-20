9bc37c611f882598fa8c7712ba25a6cf110b90a2
Roope Jourio <jourioroope@gmail.com>
2017-07-20 03:35:17 +0300

    Fixed formatting and added method compareTo to sort the dates.

--- DataStructures/Example.java
+++ DataStructures/Example.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Date;
 
 import com.teamtreehouse.Treet;
@@ -10,10 +11,23 @@ public class Example {
     "Want to be famous? Simply tweet about Java and use the hashtag #treet. I'll use your tweet in a new @treehouse couse about data structures.",
     new Date(1421849732000L)
     );
+
+    Treet secondTreet = new Treet(
+    "journeytocode",
+    "@treehouse makes learning Java soooo fun! #treet",
+    new Date(1421878767000L)
+    );
+
     System.out.printf("This is a new Treet: %s %n", treet);
     System.out.println("Words are: ");
     for (String word : treet.getWords()) {
       System.out.println(word);
+    }
+
+    Treet[] treets = {secondTreet, treet};
+    Arrays.sort(treets);
+    for (Treet exampleTreet : treets) {
+      System.out.println(exampleTreet);
     }
   }
 }
