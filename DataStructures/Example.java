67aa774dbff3daf2edf807b3fc91118c5398bf48
Roope Jourio <jourioroope@gmail.com>
2017-07-20 06:40:57 +0300

    Serialiazation shit

--- DataStructures/Example.java
+++ DataStructures/Example.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 import java.util.Date;
 
 import com.teamtreehouse.Treet;
+import com.teamtreehouse.Treets;
 
 public class Example {
 
   public static void main(String[] args) {
+
     Treet treet =  new Treet(
     "craigsdennis",
     "Want to be famous? Simply tweet about Java and use the hashtag #treet. I'll use your tweet in a new @treehouse couse about data structures.",
@@ -28,6 +30,11 @@ public class Example {
     Arrays.sort(treets);
     for (Treet exampleTreet : treets) {
       System.out.println(exampleTreet);
+    }
+    Treets.save(treets);
+    Treet[] reloadedTreets = Treets.load();
+    for (Treet reloaded : reloadedTreets) {
+      System.out.println(reloaded);
     }
   }
 }
